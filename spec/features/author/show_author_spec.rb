4fef0ae1827e5f06dc59db840ff764e71df6b84b
Gritta Weisheit <gritta.weisheit@giz.berlin>
2019-10-30 23:35:42 +0100

    added details page

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -1,1 +1,17 @@
+require 'rails_helper'
 
+describe "author detail page", type: :feature do
+
+  it "should render without error" do
+    @alan = FactoryBot.create :author
+    visit author_path(@alan)
+  end
+
+  it "should display details" do
+    @alan = FactoryBot.create :author
+    visit author_path(@alan)
+    expect(page).to have_text('Alan')
+    expect(page).to have_text('Turing')
+    expect(page).to have_text('http://wikipedia.de/Alan_Turing')
+  end
+end
