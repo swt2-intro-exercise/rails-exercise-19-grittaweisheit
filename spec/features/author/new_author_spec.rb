385d0b3980a31dc63c154af50b5d1c34ccc452b8
Gritta Weisheit <gritta.weisheit@giz.berlin>
2019-10-30 16:50:06 +0100

    added failing test for first name input field

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,8 @@ describe "New author page", type: :feature do
   it "should render withour error" do
     visit author_new_path
   end
+
+  it "should have first name input field" do
+    expect(author_new_path).to have_field('first_name_input')
+  end
 end
