2d3163c922fad95602985061a8f63cee651f6945
Gritta Weisheit <gritta.weisheit@giz.berlin>
2019-10-30 17:39:17 +0100

    fixed input labels

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -8,8 +8,8 @@ describe "New author page", type: :feature do
 
   it "should have a text input field for the fisrt name, last name and homepage" do
     visit new_author_path
-    expect(page).to have_field('author[first_name_input]')
-    expect(page).to have_field('author[last_name_input]')
-    expect(page).to have_field('author[homepage_input]')
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
   end
 end
