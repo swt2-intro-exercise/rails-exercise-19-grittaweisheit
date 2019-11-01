72d016ddff22291becf8d9dcf9ffdef51b5fe6a5
Gritta Weisheit <gritta.weisheit@giz.berlin>
2019-11-01 12:53:58 +0100

    added test and feature for deleting from index page

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -12,15 +12,25 @@ describe "index listing all authors" do
   end
 
   it "should have a link to create a new author" do
+    @alan = FactoryBot.create :author
     visit authors_path
     expect(page).to have_link 'New', href: new_author_path
   end
 
-  it "should have links to details an editing every author" do
+  it "should have links to details, destroying and editing every author" do
     @alan = FactoryBot.create :author
     visit authors_path
     expect(page).to have_link 'Show', href: author_path(@alan)
     expect(page).to have_link 'Edit', href: edit_author_path(@alan)
+    expect(page).to have_link 'Destroy', href: author_path(@alan)
+  end
+
+  it "should delete an author when told so" do
+    @alan = FactoryBot.create :author
+    visit authors_path
+    @count = Author.count
+    find(:xpath, "//tr[" + (@alan.id).to_s + "]/td[5]/a").click
+    expect(Author.count).to eq(@count-1)
   end
 
 end
