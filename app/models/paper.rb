0672c0112208f300667d29ee5924edd9542a976a
Gritta Weisheit <gritta.weisheit@giz.berlin>
2019-11-01 15:38:25 +0100

    added filtering to papers index page

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,5 +1,9 @@
 class Paper < ApplicationRecord
   has_and_belongs_to_many :authors
+
+  scope :published_in, ->(number) {where('year == ?', number)}
+
   validates :title, :venue, :year, presence: true
   validates :year, numericality: true
+
 end
