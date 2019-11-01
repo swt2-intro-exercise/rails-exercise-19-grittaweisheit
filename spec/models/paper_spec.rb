f87c4ae5a0fb245a9a759b33add96eb92befcd7b
Gritta Weisheit <gritta.weisheit@giz.berlin>
2019-11-01 13:46:46 +0100

    validation of paper params

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe Paper, type: :model do
     expect(paper.venue).to eq('conference')
     expect(paper.year).to eq(2018)
   end
+
+  it "should be invalid without a title, year (as number) or venue" do
+    paper_without_title = Paper.new(venue: 'conference', year: 2018)
+    paper_without_venue = Paper.new(title:'Superduper Paper', year: 2018)
+    paper_without_year = Paper.new(title:'Superduper Paper', venue: 'conference')
+    paper_with_nonumeric_year = Paper.new(title:'Superduper Paper', venue: 'conference', year: 'text')
+
+    expect(paper_without_title).to be_invalid
+    expect(paper_without_venue).to be_invalid
+    expect(paper_without_year).to be_invalid
+    expect(paper_with_nonumeric_year).to be_invalid
+  end
 end
